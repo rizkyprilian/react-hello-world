5e6856c3395bfff22eebf30fea9d46cc8357d20d
rizkyprilian <rizky.prilian@gmail.com>
2017-03-08 15:57:19 +0700

    Initial commit - Setup workflow with webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  entry: {
+    app: './app',
+  },
+  output: {
+    path: __dirname,
+    filename: '[name].js',
+    chunkFileName: '[id].js',
+  },
+  devServer: {
+    hot: true,
+    inline: true,
+    colors: true,
+    historyApiFallback: true,
+  },
+  resolve: {
+    modulesDirectory: ['node_modules', 'lib', 'app', 'vendor'],
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style!css',
+      },
+      {
+        test: /\.(scss|sass)$/,
+        loaders: [
+          'style',
+          'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]!sass',
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'stage-0', 'react'],
+          plugins: ['transform-runtime', 'transform-decorators-legacy'],
+        },
+      },
+    ],
+    sassLoader: {
+      includePaths: ['./vendor'],
+    },
+    plugins: [],
+  },
+};
